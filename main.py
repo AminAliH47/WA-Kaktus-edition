5aadf7c014a29e1866f73fe5df7f86943de926bf
AminAlih47 <AminAlih47@gmail.com>
2022-07-16 21:18:24 +0430

    GTmetrix Updated, bug fixed!

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from PIL import Image
 
 # Config Important Options for Webdriver
 option = webdriver.ChromeOptions()
-
-
-# option.add_argument('--headless')
+option.add_argument('--headless')
 
 
 class Analyze:
@@ -21,6 +19,7 @@ class Analyze:
         self.main_url = main_url
         self.name = name
         self.webdriver_path = webdriver_path
+        self.protocol = 'https' if 'https' in main_url else 'http'
         self.saved_path = saved_path
         self.driver = webdriver.Chrome(self.webdriver_path, options=option)
 
@@ -33,63 +32,56 @@ class Analyze:
     def get_responsive(self):
         driver = self.driver
 
-        if "https" in self.main_url:
-            # Get Responsive website URL
-            driver.get("https://ui.dev/amiresponsive")
+        protocol = self.protocol  # protocol type
 
-            # Change window size for image size
-            driver.set_window_size(1300, 700)
+        # Hash map to hold reference data
+        hash_map = {
+            'https_address': "https://ui.dev/amiresponsive",
+            'http_address': "https://amiresponsive.co.uk/",
+            'https_search': '//*[@id="url"]',
+            'http_search': '//input[@name="site"]',
+            'https_sleep': 15,
+            'http_sleep': 10,
+            'https_size': (170, 30, 1230, 700),
+            'http_size': (150, 150, 1150, 680),
+        }
 
-            # Find searchbar in page
-            search_bar = self.driver.find_element(By.XPATH, '//*[@id="url"]')
+        # Get Responsive website URL
+        driver.get(hash_map[protocol + '_address'])
 
-            # Pass Main URL to responsive website
-            search_bar.send_keys(self.main_url)
-            search_bar.send_keys(Keys.RETURN)
+        # Change window size for image size
+        driver.set_window_size(1300, 700)
 
+        # Find searchbar in page
+        search_bar = driver.find_element(By.XPATH, hash_map[protocol + '_search'])
+
+        # Pass Main URL to responsive website
+        search_bar.send_keys(self.main_url)
+        search_bar.send_keys(Keys.RETURN)
+
+        if protocol == 'https':
             # Turn background to Light
             dark_mode_btn = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/button')
             dark_mode_btn.click()
 
-            # Fixing image for good picture by changing style
-            sleep(1)
-            driver.execute_script("window.scrollTo({top:70, left:0, behavior: 'smooth'})")
-            driver.execute_script("document.body.style.zoom='110%'")
-
-            # Save file
-            sleep(15)
-            driver.save_screenshot(f"{self.saved_path}/responsive.png")
-
-            # Crop and save the image
-            image = Image.open(f"{self.saved_path}/responsive.png")
-            image.crop((170, 30, 1230, 700)).save(f"{self.saved_path}/responsive.png")
-
-        elif "http" in self.main_url:
-            # Get Responsive website URL
-            driver.get("https://amiresponsive.co.uk/")
-
-            # Change window size for image size
-            driver.set_window_size(1300, 700)
-
-            # Find searchbar in page
-            search_bar = self.driver.find_element(By.XPATH, '//input[@name="site"]')
-
-            # Pass Main URL to responsive website
-            search_bar.send_keys(self.main_url)
-            search_bar.send_keys(Keys.RETURN)
-
-            # Fixing image for good picture by changing style
+        elif protocol == 'http':
+            # make page for good picture by removing element
             sleep(1)
             driver.execute_script('document.querySelector([role="main"]).style.background = "#fff"')
             driver.execute_script('document.querySelector(".devices blockquote").remove()')
 
-            # Fixing image for good picture by changing style
-            driver.execute_script("window.scrollTo({top:70, left:0, behavior: 'smooth'})")
-            driver.execute_script("document.body.style.zoom='110%'")
+        # Fixing image for good picture by changing style
+        sleep(2)
+        driver.execute_script("window.scrollTo({top:70, left:0, behavior: 'smooth'})")
+        driver.execute_script("document.body.style.zoom='110%'")
 
-            # Save file
-            sleep(10)
-            driver.save_screenshot(f"{self.saved_path}/responsive.png")
+        # Save file
+        sleep(hash_map[protocol + '_sleep'])
+        driver.save_screenshot(f"{self.saved_path}/responsive.png")
+
+        # Crop and save the image
+        image = Image.open(f"{self.saved_path}/responsive.png")
+        image.crop(hash_map[protocol + '_size']).save(f"{self.saved_path}/responsive.png")
 
         return print("Responsive Done!")
 
@@ -108,26 +100,28 @@ class Analyze:
         login_btn = driver.find_element(By.XPATH, '//*[@id="user-nav-login"]/a')
         login_btn.click()
         # Find email and password field in page
-        email = self.driver.find_element(By.XPATH, '//input[@name="email"]')
-        password = self.driver.find_element(By.XPATH, '//input[@name="password"]')
-        submit_btn = self.driver.find_element(By.XPATH,
-                                              '//*[@id="menu-site-nav"]/div[2]/div[1]/form/div[4]/button'
-                                              )
+        email = driver.find_element(By.XPATH, '//input[@name="email"]')
+        password = driver.find_element(By.XPATH, '//input[@name="password"]')
+        submit_login_btn = driver.find_element(By.XPATH,
+                                               '//*[@id="menu-site-nav"]/div[2]/div[1]/form/div[4]/button'
+                                               )
         # Pass Main URL to responsive website
         email.send_keys("AminAlih47@gmail.com")
         password.send_keys("$Kwt!G9GxvUaY%e")
-        submit_btn.click()
+        submit_login_btn.click()
 
         sleep(2)
         # Find searchbar in page
-        search_bar = self.driver.find_element(By.XPATH, '//input[@name="url"]')
+        search_bar = driver.find_element(By.XPATH,
+                                         '/html/body/div[1]/main/article/form/div[1]/div[1]/div/input'
+                                         )
 
         # Pass Main URL to responsive website
         search_bar.send_keys(self.main_url)
         search_bar.send_keys(Keys.RETURN)
 
         # Fixing image for good picture by changing style
-        sleep(21)
+        sleep(31)
         driver.execute_script("window.scrollTo({top:80, left:0, behavior: 'smooth'})")
         driver.execute_script("document.body.style.zoom='90%'")
 
@@ -139,6 +133,9 @@ class Analyze:
     def get_backlinks(self):
         driver = self.driver
 
+        # Delete All Cookies
+        driver.delete_all_cookies()
+
         # Get Responsive website URL
         driver.get("https://lxrmarketplace.com/seo-inbound-link-checker-tool.html")
 
@@ -146,7 +143,7 @@ class Analyze:
         driver.set_window_size(1300, 700)
 
         # Find searchbar in page
-        search_bar = self.driver.find_element(By.XPATH, '//input[@name="url"]')
+        search_bar = driver.find_element(By.XPATH, '//input[@name="url"]')
 
         # Pass Main URL to responsive website
         search_bar.send_keys(self.main_url)
@@ -161,7 +158,7 @@ class Analyze:
         driver.execute_script("window.scrollTo({top:30, left:0, behavior: 'smooth'})")
 
         # Save file
-        sleep(10)
+        sleep(3)
         driver.save_screenshot(f"{self.saved_path}/backlinks.png")
 
         return print("Backlinks Done!")
