3a9bbcecc96338729c8fb3a6015dc9f5a691ae0e
Sadra Sadeghian <sadrasadeghian82@gmail.com>
2022-07-16 23:42:46 +0430

    Crop for backlinks applied.

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ url = input("Please Enter Company URL: \n")
 
 sadra_paths = ["F:\GitHub\Automation", "F:\GitHub\Automation\chromedriver.exe"]  # Don't touch this line. (NEVER)
 
-anz = Analyze(url, "مشهد صنعت")
+anz = Analyze(url, "مشهد صنعت", "F:\GitHub\Automation", "F:\GitHub\Automation\chromedriver.exe")
 
 anz.get_responsive()
