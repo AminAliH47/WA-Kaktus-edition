a00b9c19a279b4fae92688ef1489f6b2a0134c35
AminAlih47 <AminAlih47@gmail.com>
2022-07-23 10:56:41 +0430

    Update

--- run.py
+++ run.py
@@ -6,17 +6,17 @@ name = "شهدین ۲"
 SADRA_PATH = ["F:/GitHub/Automation", "F:/GitHub/Automation/chromedriver.exe"]  # Don't touch this line. (NEVER)
 AMIN_PATH = ["C:/Users/amina/OneDrive/Documents/Kaktus", "C:/Users/amina/Downloads/chromedriver.exe"]
 
-anz = Analyze(url, name)
+anz = Analyze(url)
 
 # Create directory
 anz.create_directory()
 
-# anz.get_whois()
-# anz.get_responsive()
-# anz.get_gtmetrix()
+anz.get_whois()
+anz.get_responsive()
+anz.get_gtmetrix()
 anz.get_backlinks()
-# anz.get_amp()
-# anz.get_ssl()
+anz.get_amp()
+anz.get_ssl()
 
 # Close Driver After Analyze
 anz.driver.close()
