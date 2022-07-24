58d21e4b0aa273b5b27fd21775eebd2043ee8a50
AminAlih47 <AminAlih47@gmail.com>
2022-07-25 02:35:19 +0430

    Bug Fixed!

--- run.py
+++ run.py
@@ -1,25 +1,40 @@
 from main import Analyze
 import time
 
-url = input("Please Enter Company URL: \n")
-name = "شهدین ۲"
+# Get Website URL
+url = input("Please Enter Website URL: \n")
+
+# Get Website Name
+name = input("Please Enter Website Name: (Optional) \n")
+name = "Analyze" if name == "" else name
 
 SADRA_PATH = ["F:/GitHub/Automation", "F:/GitHub/Automation/chromedriver.exe"]  # Don't touch this line. (NEVER)
 AMIN_PATH = ["C:/Users/amina/OneDrive/Documents/Kaktus", "C:/Users/amina/Downloads/chromedriver.exe"]
 
-anz = Analyze(url)
+anz = Analyze(url, name)
 
 # Create directory
 anz.create_directory()
 
 s = time.time()
 
-# anz.get_whois()
+anz.get_whois()
 anz.get_responsive()
-# anz.get_gtmetrix()
-# anz.get_backlinks()
-# anz.get_amp()
-# anz.get_ssl()
+anz.get_gtmetrix()
+anz.get_backlinks()
+anz.get_amp()
+anz.get_ssl()
+
+# Optimize Images
+while True:
+    optimize = input("\nDo you want to optimize images? (y/n) \n").lower()
+    if optimize == "y":
+        anz.optimize()
+        break
+    elif optimize == "n":
+        break
+    else:
+        print("Please enter correct value")
 
 e = time.time()
 print(f'Done in {int(e-s)} seconds.')
